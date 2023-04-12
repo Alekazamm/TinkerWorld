9b92195e039667833c865a0003523bd7cb8121c7
drdaen <daniel.ilinov@yahoo.com.au>
2023-04-12 18:27:52 +1000

    Countdown

---
+++ TinkerWorld/Assets/Scripts/Countdown.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Countdown : MonoBehaviour
+{
+    [SerializeField]
+    public TextMeshProUGUI text;
+
+    public void Start()
+    {
+        
+        StartCoroutine(Countdowns());
+    }
+
+
+    private IEnumerator Countdowns()
+    {
+        text.SetText("3");
+        yield return new WaitForSeconds(1);
+        text.SetText("2");
+        yield return new WaitForSeconds(1);
+        text.SetText("1");
+        yield return new WaitForSeconds(1);
+        text.SetText("");
+        Lava();
+        yield return null;
+        
+    }
+
+    public void Lava()
+    {
+
+    }
+
+}
