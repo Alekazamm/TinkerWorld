c3fc6c6044d2a9c837fa8af194732e1f431416a5
Alekazamm <108663087+Alekazamm@users.noreply.github.com>
2023-04-13 12:14:29 +1000

    in game menu

--- TinkerWorld/Assets/Scripts/InGameMenuScript.cs
+++ TinkerWorld/Assets/Scripts/InGameMenuScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class InGameMenuScript : MonoBehaviour
 {
+
+    public GameObject pauseButton, resumeButton;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        resumeButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        resumeButton.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        resumeButton.SetActive(false);
+        pauseButton.SetActive(true);
     }
 }
