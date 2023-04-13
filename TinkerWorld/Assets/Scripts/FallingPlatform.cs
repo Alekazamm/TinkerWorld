89806f4aed6a300a1f31846b464bc13930637a9d
drdaen <daniel.ilinov@yahoo.com.au>
2023-04-13 11:55:23 +1000

    falling plat

---
+++ TinkerWorld/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrainButton : MonoBehaviour
+{
+    public GameObject lava;
+    public GameObject fallingPlatform1, fallingPlatform2, fallingPlatform3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
