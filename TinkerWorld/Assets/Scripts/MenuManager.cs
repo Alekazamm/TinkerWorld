7d64ba4e5252684e2bfd204010a389a6e7bc0e74
Alekazamm <108663087+Alekazamm@users.noreply.github.com>
2023-04-13 14:40:45 +1000

    level select

--- TinkerWorld/Assets/Scripts/MenuManager.cs
+++ TinkerWorld/Assets/Scripts/MenuManager.cs
@@ -35,7 +35,21 @@ public class MenuManager : MonoBehaviour
         levelSelectGroup.SetActive(true);
     }
 
-    
-    
-        
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
 }
